294fc729c79ec5fac8e1924081eee58da64127e3
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-09-12 18:09:12 -0300

    Currency converter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useEffect, useState } from "react";
+// import "./style.css";
+
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+        );
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching movies");
+
+        const data = await res.json();
+
+        setConverted(data.rates[toCur]);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (fromCur === toCur) return setConverted(amount);
+
+    fetchData();
+  }, [amount, fromCur, toCur]);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {Number(converted).toFixed(2)} {toCur}
+      </p>
     </div>
   );
 }
